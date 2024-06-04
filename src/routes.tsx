032443cb113580668e8ef078da09d786aa3b192f
shermaine sng <shersmallsng@gmail.com>
2024-06-04 14:41:51 +0800

    rebuild

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,10 @@
-import Wrapper from './components/MainContainer';
-import { useAuthContext } from './context/AuthContext';
-import Login from './pages/login';
-import Signup from './pages/signup';
-import {Routes, Route, Navigate, BrowserRouter} from 'react-router-dom'
-import Tabs from './components/Tabs';
-import Link from 'next/link'
+// import Wrapper from './components/MainContainer';
+// import { useAuthContext } from './context/AuthContext';
+// import Login from './authPages/login';
+// import Signup from './authPages/signup';
+// import {Routes, Route, Navigate, BrowserRouter} from 'react-router-dom'
+// import Tabs from './components/Tabs';
+// import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
 
